3c3372f8b85807d94c7aa761b5312aee395b8925
Suprathika <93345887+SuprathikaKodi@users.noreply.github.com>
2024-03-16 00:04:17 +0530

    Update Sidebar.jsx

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,38 +2,36 @@ import React, { useState } from 'react';
 import './Sidebar.css'
 import logo from '../../imgs/logo.png'
 import { SidebarData } from '../../Data/Data';
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
-    
-    const[selected, setSelected] =useState(0)
-
-  return (
-    <div className="Sidebar">
-        {/* logo*/} 
-        <div className="logo">
-            <img src={logo} alt="logo"/>
-            <span>LogisticSystem</span>
-        </div>
-        {/*menu*/}
-        
-        <div className="menu">
-            
-            {SidebarData.map((item,index)=>{
-                return(
-                    <div className={selected===index?'menuItem active':'menuItem'}
-                    key={index}
-                    onClick={()=>setSelected(index)}
-                    >
+    const [selected, setSelected] = useState(0);
+    return (
+        <>
+        <div className="Sidebar">
+            {/* logo*/} 
+            <div className="logo">
+                <img src={logo} alt="logo"/>
+                <span>LogisticSystem</span>
+            </div>
+            {/*menu*/}
+            <div className="menu">
+                {SidebarData.map((item, index) => (
+                    <div
+                        className={selected === index ? 'menuItem active' : 'menuItem'}
+                        key={index}
+                        onClick={() => setSelected(index)}
+                        >
                         <span>
-                            {item.heading}
+                            <Link to={item.link}>{item.heading}</Link>
                         </span>
                     </div>
-                )
-            })
-        }
+                ))}
+            </div>
         </div>
-    </div>
-  ); 
+        
+        </>
+    ); 
 }
 
 export default Sidebar;
